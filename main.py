f559146e3f75c07763c4e2132167a8c1c26ddedf
daosRage <r1a2g3e4@gmail.com>
2023-11-04 16:13:06 +0200

    3

--- main.py
+++ main.py
@@ -6,13 +6,49 @@ window = pygame.display.set_mode((setting_win["WIDTH"], setting_win["HEIGHT"]))
 def run():
     game = True
 
-    hero = Hero(10,10,50,50)
+    hero = Hero(10,
+                10,
+                setting_hero["WIDTH"],
+                setting_hero["HEIGHT"], 
+                image= hero_image_list)
+    bot1 = Bot(200,
+               100, 
+               setting_bot["WIDTH"],
+               setting_bot["HEIGHT"],
+               image= bot_walk_image_list,
+               vertical= True)
+    bot2 = Bot(800,
+               500, 
+               setting_bot["WIDTH"],
+               setting_bot["HEIGHT"],
+               image= bot_walk_image_list,
+               horizontal= True,
+               speed= -5)
     clock = pygame.time.Clock()
 
     while game:
+        #BACKGROUND
         window.fill((12,90,190))
+        
+        #WALL
+        for wall in wall_list:
+            pygame.draw.rect(window, (255,255,255), wall)
 
+
+        x, y = 0, 0
+        for i in range(100):
+            pygame.draw.line(window, (255,255,255), (0, y), (setting_win["WIDTH"], y))
+            pygame.draw.line(window, (255,255,255), (x, 0), (x, setting_win["HEIGHT"]))
+            x += 20
+            y += 20
+
+        #HERO
         hero.move(window)
+
+        #BOT1 & BOT2
+        bot1.move(window)
+
+        bot2.shoot(window, hero)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
